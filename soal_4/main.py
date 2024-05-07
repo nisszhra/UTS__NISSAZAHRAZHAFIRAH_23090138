3d89b3f981a261d19e83ebd6e15186172a890a8a
Nissa Zahra <nissazahra.212@gmail.com>
2024-05-07 18:00:25 +0700

    bismillah

--- soal_4/main.py
+++ soal_4/main.py
@@ -1,9 +1,9 @@
 import modul
 
 berat_badan = int(input("Masukan Berat Badan Anda (kg): "))
-tinggi_badan = int(input("Masukan Tinggi Badan Anda (m) : "))
+tinggi_badan = float(input("Masukan Tinggi Badan Anda (m) : "))
 
-bmi = berat_badan / tinggi_badan * tinggi_badan
+bmi = berat_badan / tinggi_badan ** 2
 
 print('-'*10, 'Laporan Perhitungan BMI Anda', '-'*10)
 print(f'Berat Badan : {berat_badan}')
